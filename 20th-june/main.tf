ddfc632751903ab80c03f3f760052e56fcd6c01e
Siddharth Sharma <siddharth54330@gmail.com>
2026-06-20 20:32:43 +0530

    Add files via upload

---
+++ 20th-june/main.tf
@@ -0,0 +1,16 @@
+terraform {
+
+  required_providers {
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.5"
+    }
+  }
+}
+
+provider "local" {}
+
+resource "local_file" "hello" {
+  filename = "hello.txt"
+  content  = "Hello, World!"
+}
